c1060e5805455eff85f3cfad732009de9e41af89
Shivam Jaiswal <shivamsam17@gmail.com>
2021-12-14 05:30:49 +0000

    Initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './App.css'
+
+function App () {
+  return (
+    <div>
+      {/* start from here */}
+      User Frontend
+    </div>
+  )
+}
+
+export default App
